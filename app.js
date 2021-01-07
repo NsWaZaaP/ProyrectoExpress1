3b7579c993dfe432de6ecb02dc28372eb7ef2002
AndresR <joranrosan@hotmail.com>
2021-01-07 14:50:27 -0500

    primer commit de prueba

---
+++ app.js
@@ -0,0 +1,16 @@
+const { publicDecrypt } = require('crypto');
+const { static } = require('express');
+//? se requieren los frameworks y las funiciones necesarias desde node.js
+const express = require('express');
+const app = express();
+//? unificar rutas para que sea mas sencillo saber en que lugar se encuentra nuestro documento
+const path = require ('path');
+
+//? se crea el localhost con puerto 3000
+app.listen(3000,()=>{
+    console.log('server listen ok');
+})
+
+//? se utiliza para recibir la ruta de archivos publicos
+const publicPath = path.resolve(__dirname,'./public')
+app.use(express.static(publicPath))
